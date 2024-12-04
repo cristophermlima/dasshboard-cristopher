75ee9f2800d366da91cde5163ef29a60a3d9a13f
Cristopher <matheuslima0251@gmail.com>
2024-12-04 20:58:40 -0300

    feat material-ui-insert

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,46 +1,6 @@
 import { Theme } from '@types'
 
 export const lighTheme: Theme = {
-  appBackground: '#060B26',
-  appColor: '#FFF',
-  appDefaultStroke: '#21497D',
-  appLogo: 'dnc-logo-white.svg',
-  appSkeletonFrom: '#060b26',
-  appSkeletonTo: '#21497D',
-  buttons: {
-    alert: 'E80000',
-    alertColor: '#FFF',
-    alertHover: '#D80000',
-    disabled: '#313649',
-    disabledColor: '#6D7B8E',
-    primary: '#0C70F',
-    primaryColor: '#FFF',
-    primaryHover: '#0061DE',
-  },
-  card: {
-    alert: '#E80000',
-    background: '#0F1535',
-    border: '#21497D',
-    success: '#008000',
-    warning: '#F7A300',
-  },
-  textInput: {
-    active: '#0F1535',
-    activeColor: '#FFF',
-    borderColor: '#21497D',
-    disabled: '#282D49',
-    disabledBorderColor: '#2E3F55',
-    disabledColor: '#58677C',
-    placeholderColor: '#89A7CE',
-  },
-  typographies: {
-    error: '#FF0202',
-    subtitle: '#89A7CE',
-    success: '#008000',
-  },
-}
-
-export const darkTheme: Theme = {
   appBackground: '#FFF',
   appColor: '#000',
   appDefaultStroke: '#E0E0E0',
@@ -76,6 +36,46 @@ export const darkTheme: Theme = {
   typographies: {
     error: '#FF0202',
     subtitle: '#666',
+    success: '#008000',
+  },
+}
+
+export const darkTheme: Theme = {
+  appBackground: '#060B26',
+  appColor: '#FFF',
+  appDefaultStroke: '#21497D',
+  appLogo: 'dnc-logo-white.svg',
+  appSkeletonFrom: '#060b26',
+  appSkeletonTo: '#21497D',
+  buttons: {
+    alert: 'E80000',
+    alertColor: '#FFF',
+    alertHover: '#D80000',
+    disabled: '#313649',
+    disabledColor: '#6D7B8E',
+    primary: '#0C70F',
+    primaryColor: '#FFF',
+    primaryHover: '#0061DE',
+  },
+  card: {
+    alert: '#E80000',
+    background: '#0F1535',
+    border: '#21497D',
+    success: '#008000',
+    warning: '#F7A300',
+  },
+  textInput: {
+    active: '#0F1535',
+    activeColor: '#FFF',
+    borderColor: '#21497D',
+    disabled: '#282D49',
+    disabledBorderColor: '#2E3F55',
+    disabledColor: '#58677C',
+    placeholderColor: '#89A7CE',
+  },
+  typographies: {
+    error: '#FF0202',
+    subtitle: '#89A7CE',
     success: '#008000',
   },
 }
