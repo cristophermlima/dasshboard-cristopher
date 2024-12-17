f7b8f644b63693bbe8badfc1c5d13725f7b51569
Cristopher Mattheus Lima <mattheuslima0251@gmail.com>
2024-12-16 22:31:06 -0300

    Feat: Avatars List Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import { CardComponent, Header } from '@/components'
+import { AvatarsList, CardComponent, Header } from '@/components'
 import { Container } from '@mui/material'
+import { currencyConverter } from '@/utils'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(1234.54),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(3334.14),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(2264.74),
+    },
+  ]
   return (
     <>
       <Header></Header>
       <Container maxWidth="lg">
         <CardComponent>CARD</CardComponent>
+        <CardComponent>
+          <AvatarsList listData={mockListData} />
+        </CardComponent>
       </Container>
       <Header />
     </>
