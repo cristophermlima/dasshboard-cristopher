2a1b9f85abb5ffebb7a5e12e642b863d7fd3143e
Cristopher Mattheus Lima <mattheuslima0251@gmail.com>
2024-12-22 16:36:36 -0300

    Feat: Login page requests

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,9 @@
+import { ChangeEvent, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { jwtDecode } from 'jwt-decode'
+import Cookies from 'js-cookie'
+
+//COMPONENTS
 import { Box, Container } from '@mui/material'
 import Grid from '@mui/material/Grid'
 import {
@@ -7,9 +13,62 @@ import {
   StyledH1,
   StyledP,
 } from '@/components'
-import { pxToRem } from '@/utils'
+
+//HOOKS
+import { useFormValidation, usePost } from '@/hooks'
+
+//UTILS
+import { jwtExpirationDateConverter, pxToRem } from '@/utils'
+
+//TYPES
+import { DecodedJWT, MessageProps, LoginData, LoginPostData } from '@/types'
 
 function Login() {
+  const navigate = useNavigate()
+  const inputs = [
+    { type: 'email', placeholder: 'Email' },
+    { type: 'password', placeholder: 'Senha' },
+  ]
+  const { data, loading, error, postData } = usePost<LoginData, LoginPostData>(
+    'login'
+  )
+  const { formValues, formValid, handleChange } = useFormValidation(inputs)
+
+  const handleMessage = (): MessageProps => {
+    if (!error) return { msg: '', type: 'success' }
+    switch (error) {
+      case 401:
+        return {
+          msg: 'Email e/ou senha invalidos',
+          type: 'error',
+        }
+      default:
+        return {
+          msg: 'Não foi possivel realizar a operação. Entre em contato com nosso suporte.',
+          type: 'error',
+        }
+    }
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    await postData({
+      email: String(formValues[0]),
+      password: String(formValues[1]),
+    })
+  }
+
+  useEffect(() => {
+    if (data?.jwt_token) {
+      const decoded: DecodedJWT = jwtDecode(data?.jwt_token)
+      Cookies.set('Aythorization', data?.jwt_token, {
+        expires: jwtExpirationDateConverter(decoded.exp),
+        secure: true,
+      })
+    }
+    if (Cookies.get('Authorization')) navigate('/home')
+  }, [data, navigate])
+
   return (
     <>
       <Box>
@@ -30,17 +89,23 @@ function Login() {
               </Box>
 
               <FormComponent
-                inputs={[
-                  { type: 'email', placeholder: 'Email' },
-                  { type: 'password', placeholder: 'Senha' },
-                ]}
+                inputs={inputs.map((input, index) => ({
+                  type: input.type,
+                  placeholder: input.placeholder,
+                  value: formValues[index] || '',
+                  onchange: (e: ChangeEvent<HTMLInputElement>) =>
+                    handleChange(index, (e.target as HTMLInputElement).value),
+                }))}
                 buttons={[
-                  { className: 'primary', type: 'submit', children: 'Login' },
+                  {
+                    className: 'primary',
+                    disabled: !formValid || loading,
+                    type: 'submit',
+                    onClick: handleSubmit,
+                    children: loading ? 'Aguarde...' : 'Login',
+                  },
                 ]}
-                message={{
-                  msg: 'Sucesso!!!',
-                  type: 'success',
-                }}
+                message={handleMessage()}
               />
             </Container>
           </Grid>
