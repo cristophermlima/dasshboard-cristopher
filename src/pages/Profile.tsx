e00a7aca687900b43337290046dfde4e7dd3df49
Cristopher Mattheus Lima <mattheuslima0251@gmail.com>
2024-12-24 22:04:52 -0300

    Feat: Profile Requests

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,27 +1,181 @@
-import { useContext } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { ChangeEvent, useContext, useEffect, useState } from 'react'
 import { AppThemeContext } from '@/contexts/AppThemeContext'
-import { Grid } from '@mui/material'
+import { Grid, Container } from '@mui/material'
 
-//COMPONENTS
-import { CardComponent, Header, StyledH2, StyledButton } from '@/components'
-import { Container } from '@mui/material'
+// COMPONENTS
+import {
+  CardComponent,
+  FormComponent,
+  Header,
+  StyledH2,
+  StyledButton,
+} from '@/components'
 
-//SERVICES
+// HOOKS
+import { useFormValidation, useGet, usePut, useDelete } from '@/hooks'
+
+// SERVICES
 import { logout } from '@/services'
+
+// TYPES
+import {
+  InputProps,
+  ProfileData,
+  ProfileEditableData,
+  MessageProps,
+} from '@/types'
+
+import Cookies from 'js-cookie'
 
 function Profile() {
   const themeContext = useContext(AppThemeContext)
+
+  // HOOKS
+  const [updateMessage, setUpdateMessage] = useState<MessageProps>({
+    type: 'success',
+    msg: '',
+  })
+
+  const clearMessage = () => {
+    setTimeout(() => {
+      setUpdateMessage({
+        type: 'success',
+        msg: '',
+      })
+    }, 3000)
+  }
+
+  const {
+    data: profileData,
+    loading: profileLoading,
+    error: profileError,
+  } = useGet<ProfileData[]>('profile')
+
+  const {
+    data: profileUpdateData,
+    putData: profilePutData,
+    loading: profileUpdateLoading,
+    error: profileUpdateError,
+  } = usePut<ProfileEditableData>('profile/update') // Corrigido para objeto único
+
+  const { deleteData: profileDeleteData, loading: profileDeleteLoading } =
+    useDelete('profile/delete')
+
+  useEffect(() => {
+    if (profileData && profileData[0]) {
+      handleChange(0, profileData[0].name)
+      handleChange(1, profileData[0].email)
+      handleChange(2, profileData[0].phone)
+    }
+  }, [profileData])
+
+  // FORM
+  const inputs: InputProps[] = [
+    { name: 'name', type: 'text', placeholder: 'Nome', required: true },
+    { name: 'email', type: 'email', placeholder: 'Email', disabled: true },
+    { name: 'phone', type: 'tel', placeholder: 'Telefone', required: true },
+  ]
+
+  const { formValues, formValid, handleChange } = useFormValidation(inputs)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    await profilePutData({
+      name: String(formValues[0]),
+      phone: String(formValues[2]),
+    })
+  }
+
+  const handleDelete = async () => {
+    if (
+      confirm(
+        'Tem certeza que deseja excluir sua conta? Se sim, certifique-se de deletar os seus leads antes.'
+      ) === true
+    ) {
+      try {
+        await profileDeleteData()
+        alert('Perfil deletado com sucesso!')
+        Cookies.remove('Authorization')
+        window.location.href = '/'
+      } catch {
+        alert(
+          'Não foi possível realizar a operação. Entre em contato com o nosso suporte.'
+        )
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (profileUpdateData !== null) {
+      setUpdateMessage({
+        msg: 'Perfil atualizado com sucesso.',
+        type: 'success',
+      })
+    } else if (profileUpdateError) {
+      setUpdateMessage({
+        msg: 'Não foi possível realizar a operação. Entre em contato com nosso suporte.',
+        type: 'error',
+      })
+    }
+    clearMessage()
+  }, [profileUpdateData, profileError, profileUpdateError])
+
   return (
     <>
-      <Header></Header>
+      <Header />
       <Container className="mb-2" maxWidth="lg">
         <Grid container spacing={4}>
           <Grid item xs={12} sm={6}>
-            <CardComponent>Seus dados....</CardComponent>
+            {!profileError && (
+              <CardComponent
+                className={
+                  profileLoading ? 'skeleton-loading skeleton-loading-mh-2' : ''
+                }
+              >
+                {!profileLoading && profileData && profileData[0] && (
+                  <>
+                    <StyledH2 className="mb1">Seus Dados</StyledH2>
+                    <FormComponent
+                      inputs={inputs.map((input, index) => ({
+                        ...input,
+                        value: formValues[index] || '',
+                        onChange: (e: ChangeEvent<HTMLInputElement>) =>
+                          handleChange(
+                            index,
+                            (e.target as HTMLInputElement).value
+                          ),
+                      }))}
+                      buttons={[
+                        {
+                          className: 'primary',
+                          disabled: !formValid || profileLoading,
+                          type: 'submit',
+                          onClick: handleSubmit,
+                          children: profileUpdateLoading
+                            ? 'Aguarde...'
+                            : 'Atualizar meu perfil',
+                        },
+                        {
+                          className: 'alert',
+                          disabled: profileDeleteLoading,
+                          type: 'button',
+                          onClick: handleDelete,
+                          children: profileDeleteLoading
+                            ? 'Aguarde...'
+                            : 'Excluir minha conta',
+                        },
+                      ]}
+                      message={updateMessage}
+                    />
+                  </>
+                )}
+              </CardComponent>
+            )}
           </Grid>
           <Grid item xs={12} sm={6}>
             <CardComponent>
-              <StyledH2 className="mb1">Definições de conta</StyledH2>{' '}
+              <StyledH2 className="mb1">Definições de conta</StyledH2>
               <StyledButton
                 className="primary mb-1"
                 onClick={themeContext?.toggleTheme}
